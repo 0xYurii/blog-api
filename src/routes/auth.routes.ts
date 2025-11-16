93911539902c09b73b052789d3324a216a3c1c96
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-11-16 22:19:07 +0100

    Add login route and import

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,8 +1,9 @@
 import Router from "express";
-import { signup } from "../controllers/auth.controller";
+import { signup, login } from "../controllers/auth.controller";
 
 const route = Router();
 
 route.post("/signup", signup);
+route.post("/login", login);
 
 export default route;
